a74fd3788ab46531029dcbab28eea13e9e08fbe8
Gaurav Dalvi <gdalvi20dec@gmail.com>
2024-10-15 20:05:29 +0530

    added svg logo and declarations file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import logo from './assets/logo.svg';
+
 function App() {
-  return <h1>webpack-react-typescript</h1>;
+  return (
+    <>
+    <img src={logo} alt="logo" style={{height: '200px'}} />
+    <h1>webpack react typescript setup</h1>
+    </>
+  )
 }
 
 export default App;
